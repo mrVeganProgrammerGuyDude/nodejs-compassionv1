cddf176d44f4ab15eb680590eb571a8407cd6fdf
Agent Broccoli <hellosheldonstewart@gmail.com>
2021-02-24 19:38:49 -0500

    testing mongo setup

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/CIAAdb', {
+
+
+mongoose.connect('mongodb+srv://broccoli:DnkiaozXrTkRti5f@cluster0.o6fdy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 })
+
+
+//OLD ONE THAT WORKS FOR LOCAL DATABASE
+// mongoose.connect('mongodb://127.0.0.1:27017/CIAAdb', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+// })
