1fd1699d45276c1dc1976fd6fa2eea8cc90fb32c
Agent Broccoli <hellosheldonstewart@gmail.com>
2021-03-09 15:14:21 -0500

    sc

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose')
 
 
-
-mongoose.connect('mongodb+srv://broccoli:VpuHsmccA15DiqkG@cluster0.o6fdy.mongodb.net/CIAAdb?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-})
-
-
-//OLD ONE THAT WORKS FOR LOCAL DATABASE
-// mongoose.connect('mongodb://127.0.0.1:27017/CIAAdb', {
+//THIS WORKS FOR LIVE MONGO DATABASE
+// mongoose.connect('mongodb+srv://broccoli:VpuHsmccA15DiqkG@cluster0.o6fdy.mongodb.net/CIAAdb?retryWrites=true&w=majority', {
 //     useNewUrlParser: true,
 //     useCreateIndex: true,
 //     useFindAndModify: false,
 //     useUnifiedTopology: true
 // })
+
+
+//OLD ONE THAT WORKS FOR LOCAL DATABASE
+mongoose.connect('mongodb://127.0.0.1:27017/CIAAdb', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}) 
